4975f9f2de321186cb1c757985a0175dd26975cb
Christian Fernandez <fernandez.christian13@gmail.com>
2023-03-31 20:01:56 -0400

    Update mlb-app.py

--- app_1_MLB_eda/mlb-app.py
+++ app_1_MLB_eda/mlb-app.py
@@ -142,10 +142,10 @@ with tab1:
         df = pd.read_csv('output.csv')
 # averages among ranking-qualified hitters across the MLB # (min.502 PA, min. 186 PA for shortened 2020 Season)
         if selected_year == 2023 :
-            qualifier = hit_stats[hit_stats['PA']]
+            qualifier = hit_stats[hit_stats['PA']>=1]
             qualified = pd.DataFrame(qualifier.mean())
             qualified.columns=['League Average per Hitter']
-            team = hit_selected_team[hit_selected_team['PA']]
+            team = hit_selected_team[hit_selected_team['PA']>=1]
             team_qualified = pd.DataFrame(team.mean())
             team_qualified.columns=[''f'{selected_team} ' 'Average per Hitter']
         if selected_year == 2020 :
